01cec338d7f4ec4be5b4445ab4ac949e535c5588
bohda <bohda@DESKTOP-KMLOK9E>
2022-02-17 19:38:01 +0200

    Add project files.

---
+++ MathLibrary/MathHalper.cs
@@ -0,0 +1,10 @@
+﻿namespace MathLibrary
+{
+    public class MathHalper
+    {
+        public static int Max(int a, int b, int c)
+        {
+            return a > b ? (a > c ? a : c) : (b > c ? b : c);
+        }
+    }
+}
